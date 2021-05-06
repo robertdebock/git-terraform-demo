e0f7a1d0c798cf8b39a5c750ebc436c96293f45d
Robert de Bock <robert@meinit.nl>
2021-05-06 08:35:23 +0200

    Disable remote backend.

--- versions.tf
+++ versions.tf
@@ -5,12 +5,12 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-  backend "s3" {
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    endpoint                    = "https://ams3.digitaloceanspaces.com"
-    region                      = "eu-west-1"
-    # bucket                      = "terraform-demo-state"
-    key                         = "terraform.tfstate"
-  }
+  # backend "s3" {
+  #   skip_credentials_validation = true
+  #   skip_metadata_api_check     = true
+  #   endpoint                    = "https://ams3.digitaloceanspaces.com"
+  #   region                      = "eu-west-1"
+  #   bucket                      = "terraform-demo-state"
+  #   key                         = "terraform.tfstate"
+  # }
 }
