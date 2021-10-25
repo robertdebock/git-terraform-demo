f5752f3f81766e95c78983c1b6671b853b618d82
Robert de Bock <robert@meinit.nl>
2021-10-25 15:24:53 +0200

    Add remote state.

--- versions.tf
+++ versions.tf
@@ -5,12 +5,10 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-  # backend "s3" {
-  #   skip_credentials_validation = true
-  #   skip_metadata_api_check     = true
-  #   endpoint                    = "https://ams3.digitaloceanspaces.com"
-  #   region                      = "eu-west-1"
-  #   bucket                      = "terraform-demo-state"
-  #   key                         = "terraform.tfstate"
-  # }
+  backend "remote" {
+    organization = "robertdebock"
+    workspaces {
+      name = "remote-state-demo"
+    }
+  }
 }
