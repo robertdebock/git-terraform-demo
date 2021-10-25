dcb28c960653e6b9c74eca0f9610929587620d2d
Robert de Bock <robert@meinit.nl>
2021-10-25 15:29:36 +0200

    Not always on.

--- versions.tf
+++ versions.tf
@@ -5,10 +5,10 @@ terraform {
     }
   }
   required_version = ">= 0.13"
-  backend "remote" {
-    organization = "robertdebock"
-    workspaces {
-      name = "remote-state-demo"
-    }
-  }
+  # backend "remote" {
+  #   organization = "robertdebock"
+  #   workspaces {
+  #     name = "remote-state-demo"
+  #   }
+  # }
 }
